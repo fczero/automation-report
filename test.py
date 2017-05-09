77fcd610468a4715cd7a1f039f2966bab89cf65d
Francis Lagadia <flagadia@sqasquared.com>
2017-05-09 16:21:28 -0700

    update build script

--- test.py
+++ test.py
@@ -1,11 +1,28 @@
+#!/usr/bin/env python3
+
 import unittest
 import ccp_daily_automation
+
+from openpyxl import Workbook
+from openpyxl.styles import colors
+from openpyxl.styles import Color, PatternFill, Font, Border, NamedStyle
+from openpyxl.styles import Alignment, Side
+from openpyxl.formatting import Rule
+from openpyxl.formatting.rule import CellIsRule
+from openpyxl.utils import get_column_letter, rows_from_range
+from openpyxl.utils import units
+from openpyxl.worksheet.datavalidation import DataValidation
+from selenium import webdriver as wd
+import json
+import requests
 
 
 class testScript(unittest.TestCase):
 
     def setUp(self):
         pass
+
+    def 
 
 if __name__ == "__main__":
     unittest.main()
